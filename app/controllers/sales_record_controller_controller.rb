b5b2d1d8f001fa4b371ba6907a0f745df09bee35
baesueon <qotndus43@naver.com>
2019-08-01 11:28:58 +0900

    add function

--- app/controllers/sales_record_controller_controller.rb
+++ app/controllers/sales_record_controller_controller.rb
@@ -4,7 +4,7 @@ class SalesRecordControllerController < ApplicationController
     @date_revenue = record.select([:created_date, :revenue]).group('created_date').sum(:revenue)
     @date_revenue = Hash[@date_revenue.sort]
   end
-
+  
   def show
     @date = params[:id]
     @datas = Hash.new
@@ -16,6 +16,20 @@ class SalesRecordControllerController < ApplicationController
          @datas[courses.find(record.course_id).title] = record.revenue
       end
     end
+    @datas = Hash[@datas.sort_by{|k, v| v}.reverse]
+  end
+
+  def result
+    @text = params[:search_text]
+
+    @searches = eval(params[:course_model]).where("title like ?", "%#{params[:search_text]}%")
+    @datas = Hash.new
+    records = SalesRecord.all
+
+    @searches.each do |search|
+      @datas[search.title] = records.find(search.id).price * search.total_count 
+    end
+    
     @datas = Hash[@datas.sort_by{|k, v| v}.reverse]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'sales_record_controller#index'
   get '/sales_record/:id' => 'sales_record_controller#show',as:'daily_sales_by_course'
   get '/course/show/:id' => 'course#show', as:'course_sales_by_date'
+  
+  get '/sales_record' => 'sales_record_controller#result', as: 'searches_result'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
